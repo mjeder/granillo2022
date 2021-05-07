0aa8188944abda8445ef450deba0283117bc8463
mjeder <matteder96@gmail.com>
2021-05-06 22:10:55 -0500

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -41,3 +41,32 @@
     });
 
 })(jQuery); // End of use strict
+
+(function () {
+  const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+  let date = "Apr 16, 2022 00:00:00",
+      countDown = new Date(date).getTime(),
+      x = setInterval(function() {
+
+        let now = new Date().getTime(),
+            distance = countDown - now;
+
+        document.getElementById("days").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+        //do something later when date is reached
+        if (distance < 0) {
+         document.getElementById("days").innerText = 0,
+          document.getElementById("hours").innerText = 0,
+          document.getElementById("minutes").innerText = 0,
+          document.getElementById("seconds").innerText = 0;
+        }
+        //seconds
+      }, 0)
+  }());
